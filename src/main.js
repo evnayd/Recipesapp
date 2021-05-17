0c67ca39f7e78eae629b87cd3dd9a11f77f3e450
evnayd <50381660+evnayd@users.noreply.github.com>
2021-05-17 22:22:28 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 
-//import BaseCard from './components/UI/BaseCard.vue';
-//import BaseButton from './components/UI/BaseButton.vue';
+vue';
 import App from './App.vue';
 
 const app = createApp(App);
 
-//app.component('base-card', BaseCard);
-//app.component('base-button', BaseButton);
+
 
 app.mount('#app');
